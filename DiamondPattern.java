f7737ad8301d6ba51e64e51056d0c23a09d953a5
smutallib97 <smutallib97@gmail.com>
2022-10-01 00:40:31 +0530

    Diamond Pattern

---
+++ DiamondPattern.java
@@ -0,0 +1,27 @@
+package designpatternsprogs;
+
+public class DiamondPattern {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 5; i++) {
+            for (int k = 3; k >= i; k--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+
+        for (int i =1; i<5; i++) {
+            for (int k = 1; k <= i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j < 5 - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
