76431d376b87a25e157091da8594b506a2073f89
smutallib97 <smutallib97@gmail.com>
2022-10-01 00:42:23 +0530

    Numbers Pattern

---
+++ NumbersPattern.java
@@ -0,0 +1,21 @@
+package designpatternsprogs;
+
+public class NumbersPattern {
+    public static void numPrints(int n) {
+        int i, j, num;
+        for (i = 0; i < n; i++) {
+            num = 1;
+            for (j = 0; j <= i; j++) {
+                System.out.print(num + " ");
+
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        numPrints(n);
+    }
+}
